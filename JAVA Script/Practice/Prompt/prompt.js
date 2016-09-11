f6d6996b7a69d3851cba4d9da2c2229b13aeeeec
Your Name <you@example.com>
2016-09-11 20:54:14 +0500

    js practice

---
+++ JAVA Script/Practice/Prompt/prompt.js
@@ -0,0 +1,4 @@
+var data =prompt ("Enter Your Name","Name");
+alert(data);
+var data1 =prompt("Enter Today's Date","Date");
+alert("Today's Date is "+data1);
---
+++ JAVA Script/Practice/alert/script.js
@@ -0,0 +1,6 @@
+alert("Hello World");
+var x=5;
+var y="10";
+var z=4;
+var abc=x+z+y;
+alert(abc)
