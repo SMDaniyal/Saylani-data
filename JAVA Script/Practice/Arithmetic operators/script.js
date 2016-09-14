d752683edb8cd9a8d5fefe2171b8528ed2820c1b
Your Name <you@example.com>
2016-09-14 21:46:36 +0500

    Arithmetic operators

---
+++ JAVA Script/Practice/Arithmetic operators/script.js
@@ -0,0 +1,36 @@
+function Addition()
+{
+var value1=parseInt(prompt("1st no"));
+var value2=parseInt(prompt("2st no"));
+var add=(value1+value2);
+alert("Addition is : " +add);
+}
+function Subtraction()
+{
+var value3=parseInt(prompt("1st no"));
+var value4=parseInt(prompt("2st no"));
+var sub=(value3-value4);
+alert("Subtraction is : " +sub);
+}
+function Multiplication()
+{
+var value3=parseInt(prompt("1st no"));
+var value4=parseInt(prompt("2st no"));
+var sub=(value3*value4);
+alert("Multiplication is : " +sub);
+}
+function Division()
+{
+var value3=parseInt(prompt("1st no"));
+var value4=parseInt(prompt("2st no"));
+var sub=(value3/value4);
+alert("Division is : " +sub);
+}
+function Mod()
+{
+var value3=parseInt(prompt("1st no"));
+var value4=parseInt(prompt("2st no"));
+var sub=(value3%value4);
+alert("Reminder is : " +sub);
+}
+
